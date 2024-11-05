330976ce97e73db1e08080f54a832286c02de3bb
Ubaid-Ur-Rehman18 <hydraulicexperts1@gmail.com>
2024-11-05 19:43:48 +0500

    Add files via upload

---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div>
+            <img src ={"Header Top.png"} />
+        </div>
+
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Header from "./components/header"
+
+export default function Home() {
+  return(
+    <div>
+      <Header />
+      {/* <div className="homepage">
+        <div className="text">
+          <h2 className="h2">pro.Beyond</h2>
+          <h1 className="h1">IPhone 14 <span className="span"> pro</span> </h1>
+          <p className="h2"> Create to change everything for better, for everyone  </p>
+          <button className="button"> <b>shop Now</b></button>
+        </div>
+      </div>
+      <div className="picture">
+        <img src={"/Iphone.png"}></img>
+      </div> */}
+      <img src={"Banner.png"}/>
+      <img src={"Smaller.png"} />
+      <img src ={"Category.png"}/>
+      <img src ={"Products.png"} />
+      <img src ={"Banners.png"} />
+      <img src = {"Discount.png"}/>
+      <img src ={"Banner 2.png"} />
+      <img src ={"Footer.png"} />
+      
+      
+
+    </div>
+  )
+}
